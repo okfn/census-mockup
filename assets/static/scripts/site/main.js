b6f93ecb8637ae6360f19608ee9c14b2ba77e5d9
Sam Smith <sam@mintcanary.com>
2016-07-27 18:38:56 +0100

    add all the scripts

--- assets/static/scripts/site/main.js
+++ assets/static/scripts/site/main.js
@@ -1,2 +1,7 @@
-define(["censusForm"], function($) {
+define(['table', 'place', 'ui', 'domReady', 'censusForm'], function(table, place, ui, domReady, censusForm) {
+    domReady(function() {
+        place.init();
+        table.init();
+        ui.init();
+    });
 });
